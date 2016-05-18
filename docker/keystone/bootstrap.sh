eb6705ec76e4c4aa260c808301f72efc5d0e3cbd
Artur Zarzycki <azarzycki@mirantis.com>
2016-05-18 18:40:30 +0200

    Fix keystone bootstrap
    Change-Id: I14b1e9041f6a4a8da5f3c547699858b369e2ff64

--- docker/keystone/bootstrap.sh
+++ docker/keystone/bootstrap.sh
@@ -7,6 +7,8 @@ sed -i "s/keystone_pass/${KEYSTONE_PASS}/g" /etc/keystone/keystone.conf
 sed -i "s/keystone_addr/${DB_HOST}/g" /etc/keystone/keystone.conf
 sed -i "s/keystone_db/${KEYSTONE_DB}/g" /etc/keystone/keystone.conf
 
+controller=`hostname -i`
+
 # bootstrap db
 mysql -u root -p${ROOT_DB_PASS} -h ${DB_HOST} -e 'create database keystone'
 mysql -u root -p${ROOT_DB_PASS} -h ${DB_HOST} -e "grant all privileges on keystone.* to '${KEYSTONE_USER}'@'%' identified by '${KEYSTONE_PASS}';"
@@ -15,10 +17,29 @@ keystone-manage db_sync
 
 keystone-manage bootstrap --bootstrap-password ${KEYSTONE_PASS}
 
+/usr/sbin/apachectl start
+
 export OS_URL=http://127.0.0.1:35357/v3
 export OS_TOKEN=adm_tok
 export OS_IDENTITY_API_VERSION=3
 
+id=`openstack service list | awk '/ identity / {print $2}' | wc -l`
 
-openstack service create --name keystone --description "OpenStack Identity" identity
+if [ "$id" -eq "0" ]
+then
+    openstack service create --name keystone --description "OpenStack Identity" identity
+
+    openstack endpoint create --region RegionOne \
+	  identity public http://$controller:5000/v3
+
+    openstack endpoint create --region RegionOne \
+	  identity internal http://$controller:5000/v3
+
+    openstack endpoint create --region RegionOne \
+	  identity admin http://$controller:35357/v3
+fi
+/usr/sbin/apachectl stop
+
+sleep 5
+
 /usr/sbin/apache2ctl -D FOREGROUND
